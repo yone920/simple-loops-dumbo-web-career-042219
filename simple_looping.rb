e66bb47f8b78ade45cb80b42cb5f3192fd6b4c53
Yonas Fesehatsion <yone920@gmail.com>
2019-04-08 16:32:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -30,7 +30,7 @@ def while_iterator(number_of_times)
   end
 end
 
-counter = 0
+counter = 10
 def until_iterator(number_of_times)
   until counter == number_of_times 
   puts "Welcome to Flatiron School's Web Development Course!"
